624cc693ac8ddb95559131d8462af9ab68700930
Raphaël Rail <rrail@recursyve.io>
2024-06-10 16:47:23 -0400

    test python ci run

--- rng.py
+++ rng.py
@@ -5,6 +5,7 @@ max_number = int(input('Please enter the max number: '))
 
 if (max_number < min_number): 
   print('Invalid input - shutting down...')
+  
 else:
   rnd_number = randint(min_number, max_number)
   print(rnd_number)
