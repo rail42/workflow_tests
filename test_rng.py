100b2415752065e4662faab21fd0e948f55d2db9
Raphaël Rail <rrail@recursyve.io>
2024-06-10 21:17:55 -0400

    fixed error

--- test_rng.py
+++ test_rng.py
@@ -5,5 +5,5 @@ class TestRng(unittest.TestCase):
     
     def test_rng(self):
         result = rng.randomFromRange(324, 1000)
-        self.assertNotEqual(-1)
+        self.assertNotEqual(result, -1)
         self.assertTrue(324 <= result <= 1000)
